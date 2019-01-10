d20ef07df6a70566c04a40deb9309b7d280b1bfb
Oliver Jägle <github@beimir.net>
2019-01-10 22:19:26 +0100

    fix uid serialization

--- plugins/user_interface/raspi_nfc_spi/index.js
+++ plugins/user_interface/raspi_nfc_spi/index.js
@@ -10,7 +10,9 @@ const MY_LOG_NAME = 'RasPi NFC plugin';
 module.exports = NFCReader;
 
 const serializeUid = function(uid){
-	return id[0].toString(16)+uid[1].toString(16)+uid[2].toString(16)+uid[3].toString(16);
+	return uid && uid[0]
+		? uid[0].toString(16)+uid[1].toString(16)+uid[2].toString(16)+uid[3].toString(16)
+		: JSON.stringify(uid);
 }
 
 function NFCReader(context) {
@@ -20,11 +22,13 @@ function NFCReader(context) {
 	self.logger = self.context.logger;
 
 	const handleCardDetected = function(uid) {
-		self.commandRouter.pushToastMessage('success', 'NFC card detected', serializeUid(uid));
+		// self.commandRouter.pushToastMessage('success', 'NFC card detected', serializeUid(uid));
+		self.logger.info('NFC card detected', serializeUid(uid));
 	}
 
 	const handleCardRemoved = function(uid) {
-		self.commandRouter.pushToastMessage('success', 'NFC card removed', serializeUid(uid));
+		// self.commandRouter.pushToastMessage('success', 'NFC card removed', serializeUid(uid));
+		self.logger.info('NFC card removed', serializeUid(uid));
 	}
 
 	const spiChannel = 0; //TODO: configure SPI channel
